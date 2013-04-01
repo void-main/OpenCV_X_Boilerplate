14291b9054656bd0feacd3f63fd6bfdf7d77248e
Void Main <voidmain1313113@gmail.com>
2013-04-01 21:39:42 +0800

    finished boilerplate code

--- OpenCV_Boilerplate/main.cpp
+++ OpenCV_Boilerplate/main.cpp
@@ -5,14 +5,31 @@
 //  Created by Sun Peng on 4/1/13.
 //  Copyright (c) 2013 Void Main Studio. All rights reserved.
 //
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cvaux.hpp>
 
-#include <iostream>
-
-int main(int argc, const char * argv[])
+int main(int argc, char** argv)
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    IplImage * pInpImg = 0;
+    
+    // Load an image from file - change this based on your image name
+    pInpImg = cvLoadImage("my_image.jpg", CV_LOAD_IMAGE_UNCHANGED);
+    if(!pInpImg)
+    {
+        fprintf(stderr, "failed to load input image\n");
+        return -1;
+    }
+    
+    // Write the image to a file with a different name,
+    // using a different image format -- .png instead of .jpg
+    if( !cvSaveImage("my_image_copy.png", pInpImg) )
+    {
+        fprintf(stderr, "failed to write image file\n");
+    }
+    
+    // Remember to free image memory after using it!
+    cvReleaseImage(&pInpImg);
+    
     return 0;
 }
-
