a2a3bcab39188a6e8f38cceb49eae4f065d2a754
Void Main <voidmain1313113@gmail.com>
2013-04-01 19:25:36 +0800

    Initial Commit

---
+++ OpenCV_Boilerplate/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCV_Boilerplate
+//
+//  Created by Sun Peng on 4/1/13.
+//  Copyright (c) 2013 Void Main Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
